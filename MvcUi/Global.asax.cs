764726986d38750042ba53c63c748ef73168ee17
pauluxxx <pauluxxx@mail.ru>
2017-03-18 09:59:15 +0200

    adding a NinjectControllerFactory for DependenciInjection.

--- MvcUi/Global.asax.cs
+++ MvcUi/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcUi.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,17 +9,21 @@ using System.Web.Routing;
 
 namespace MvcUi
 {
+    //нужно ли самому настраиваить injects для NinjectHttpModule,OnePerRequestHttpModule,Bootstrapper 
     public class MvcApplication : System.Web.HttpApplication
     {
+        /// <summary>
+        ///NLog comit a log into system directiry it's have no need to be injected
+        /// </summary>
         private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
         protected void Application_Start()
         {
             logger.Info("Application Start");
             AreaRegistration.RegisterAllAreas();
-
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
         }
         public override void Init()
         {
---
+++ MvcUi/Infrastructure/CustomErrorHandlerAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace MvcUi.Infrastructure
+{
+    internal class CustomErrorHandlerAttribute : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            filterContext.ExceptionHandled = true;
+
+            if (filterContext.HttpContext.Request.IsAjaxRequest())
+            {
+                filterContext.HttpContext.Response.Clear();
+                filterContext.HttpContext.Response.Status = "500 Internal Server Error";
+                filterContext.Result = new JsonResult { Data = new { ErrorMessage = filterContext.Exception.Message } };
+
+            }
+            else
+            {
+                string actionName = filterContext.RouteData.Values["action"].ToString();
+                string controllerName = filterContext.RouteData.Values["controller"].ToString();
+                var model = new System.Web.Mvc.HandleErrorInfo(filterContext.Exception, controllerName, actionName);
+                filterContext.Result = new ViewResult()
+                {
+                    ViewName = "Error",
+                    ViewData = new ViewDataDictionary(model)
+                };
+            }
+            
+        }
+    }
+}
