85f0d554158d0529b53667198c69855bf07fedfe
pauluxxx <pauluxxx@mail.ru>
2017-03-18 09:58:25 +0200

    Add Nlog, PreStartActivator for futher adding a method to init db for others contributors.

---
+++ MvcUi/App_Start/PreStartAppDBCreate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(MvcUi.App_Start.PreStartAppDBCreate), "Start")]
+namespace MvcUi.App_Start
+{
+     public class PreStartAppDBCreate
+    {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        /// <summary>
+        /// Метод запускается один раз перед стартом приложения        
+        /// </summary>
+        public static void Start()
+        {
+            logger.Info("Application PreStart");
+        }
+    }
+}
